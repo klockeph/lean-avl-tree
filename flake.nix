8a490287385fbfc74daf67dbce6c4bafb0f887fc
Philipp Klocke <philipp.a.klocke@gmail.com>
2026-07-29 11:43:27 +0200

    basic nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Dependencies";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            elan
+          ];
+        };
+      }
+    );
+}
